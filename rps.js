88c96d7eb5d416946316b5db2242c743433f884f
jakeadam10 <jake.adam@ada.ac.uk>
2024-09-25 14:02:32 +0000

    Done with ternary  operators

--- rps.js
+++ rps.js
@@ -2,17 +2,16 @@ function rockPaperScissors(player1, player2) {
   const winningMove = {
     rock: ["lizard", "scissors"],
     paper: ["rock", "spock"],
-    scissors: ["lizard", "paper"],  
+    scissors: ["lizard", "paper"],
     lizard: ["paper", "spock"],
     spock: ["rock", "scissors"],
   };
 
-  if (player1 === player2) {
-    return "draw";
-  } else if (winningMove[player1].includes(player2)) {
-    return "player1";
-  } 
-  return "player2";
+  return player1 === player2
+    ? "draw"
+    : winningMove[player1].includes(player2)
+    ? "player1"
+    : "player2";
 }
 
 // Leave this code here for the automated tests
