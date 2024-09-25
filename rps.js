2c77ac4ec396de1be46b724e1e95cd17a3c4223b
jakeadam10 <jake.adam@ada.ac.uk>
2024-09-25 11:39:51 +0000

    Visual changes for readability and 'glanceability'

--- rps.js
+++ rps.js
@@ -1,7 +1,8 @@
 function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
-    return "draw";
-  } else if (player1 === "scissors" && player2 === "paper") {
+    return "draw"; } 
+
+  else if (player1 === "scissors" && player2 === "paper") {
     return "player1";
   } else if (player1 === "paper" && player2 === "rock") {
     return "player1";
@@ -20,8 +21,9 @@ function rockPaperScissors(player1, player2) {
   } else if (player1 === "scissors" && player2 === "lizard") {
     return "player1";
   } else if (player1 === "spock" && player2 === "rock") {
-    return "player1";
-  } else {
+    return "player1"; } 
+    
+    else {
     return "player2";
   }
 }
