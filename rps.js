0bd2b62b9c8971abe893b2767ab56daa450a76a4
jakeadam10 <jake.adam@ada.ac.uk>
2024-09-25 13:41:48 +0000

    DONE

--- rps.js
+++ rps.js
@@ -1,19 +1,18 @@
 function rockPaperScissors(player1, player2) {
-  const winConditions = {
+  const winningMove = {
     rock: ["lizard", "scissors"],
     paper: ["rock", "spock"],
-    scissors: ["lizard", "paper"],
+    scissors: ["lizard", "paper"],  
     lizard: ["paper", "spock"],
     spock: ["rock", "scissors"],
   };
 
   if (player1 === player2) {
     return "draw";
-  } else if (winConditions[player1].includes(player2)) {
+  } else if (winningMove[player1].includes(player2)) {
     return "player1";
-  } else {
-    return "player2";
-  }
+  } 
+  return "player2";
 }
 
 // Leave this code here for the automated tests
