74c0abe1b06bbb6474582bf7e5bd56e9fdb2efe3
jakeadam10 <jake.adam@ada.ac.uk>
2024-09-25 11:27:31 +0000

    first completion

--- rps.js
+++ rps.js
@@ -8,11 +8,24 @@ function rockPaperScissors(player1, player2) {
     return 'player1';
   } else if (player1 === 'rock' && player2 === 'scissors'){
     return 'player1';
-  } else {
+  } else if (player1 === 'lizard' && player2 === 'spock') {
+    return 'player1'
+  } else if (player1 === 'spock' && player2 === 'scissors') {
+    return 'player1'
+  } else if (player1 === 'rock' && player2 === 'lizard') {
+    return 'player1'
+  } else if (player1 === 'paper' && player2 === 'spock') {
+    return 'player1'
+  } else if (player1 === 'lizard' && player2 === 'paper') {
+    return 'player1'
+  } else if (player1 === 'scissors' && player2 === 'lizard') {
+    return 'player1'
+  }  else if (player1 === 'spock' && player2 === 'rock') {
+    return 'player1'
+  }
+  else {
     return 'player2'
   }
-
-   
 }
 
 
