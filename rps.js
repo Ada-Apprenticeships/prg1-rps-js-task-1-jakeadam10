a35e488f8661451f6ef42ba83f8e3e1188239b08
jakeadam10 <jake.adam@ada.ac.uk>
2024-09-25 09:57:39 +0000

    standard rps logic done (i think)

--- rps.js
+++ rps.js
@@ -1,6 +1,21 @@
 function rockPaperScissors(player1, player2) {
 
-  
+  if (player1 === player2){
+    return 'draw';
+  } else if (player1 === 'scissors' && player2 === 'paper'){ 
+    return 'player1';
+  } else if (player1 === 'scissors' && player2 === 'rock'){ 
+    return 'player2';
+  } else if (player1 === 'paper' && player2 === 'rock'){ 
+    return 'player1';
+  } else if (player1 === 'paper' && player2 === 'scissors'){
+    return 'player2';
+  } else if (player1 === 'rock' && player2 === 'paper'){
+    return 'player2';
+  } else if (player1 === 'rock' && player2 === 'scissors'){
+    return 'player1';
+  }
+
    
 }
 
